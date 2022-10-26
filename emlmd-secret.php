f03b51d7383585f13633d6ff1c64524a28a892ab
Alex Agranov <alex.agranov@audiocodes.com>
2022-10-26 14:16:46 +0300

    ﻿improve secret code verification

--- emlmd-secret.php
+++ emlmd-secret.php
@@ -21,7 +21,7 @@ class EM_Limmud_Secret {
 		return $valid;
 	}
 
-    public static function check_code_unique($secret_code, $EM_Booking) {
+    public static function check_code_unique($secret_code, $first_name, $last_name, $EM_Booking) {
 		// check if code was already used
 		foreach ($EM_Booking->get_event()->get_bookings()->bookings as $EM_OtherBooking) {
 			if ($EM_Booking->booking_id == $EM_OtherBooking->booking_id) {
@@ -31,11 +31,23 @@ class EM_Limmud_Secret {
 			foreach($EM_OtherBooking->get_tickets_bookings()->tickets_bookings as $EM_Ticket_Booking) {
 				if (!empty($attendees_data[$EM_Ticket_Booking->ticket_id])) {
 					foreach($attendees_data[$EM_Ticket_Booking->ticket_id] as $attendee_title => $attendee_data) {
+						$attendee_first_name = '';
+						$attendee_last_name = '';
+						$attendee_secret_code = '';
 						foreach( $attendee_data as $attendee_label => $attendee_value) {
 							$label = apply_filters('translate_text', $attendee_label, 'ru');
-							if (($label == 'Секретный код') && ($attendee_value == $secret_code)) {
-								return false;
+							if ($label == 'Имя (на английском)') {
+								$attendee_first_name = $attendee_value;
 							}
+							if ($label == 'Фамилия (на английском)') {
+								$attendee_last_name = $attendee_value;
+							}
+							if ($label == 'Секретный код') {
+								$attendee_secret_code = trim($attendee_value);
+							}
+						}
+						if (($attendee_secret_code == $secret_code) && (($attendee_first_name != $first_name) || ($attendee_last_name != $last_name))) {
+							return false;
 						}
 					}
 				}
@@ -75,13 +87,21 @@ class EM_Limmud_Secret {
 				foreach($attendees_data[$EM_Ticket_Booking->ticket_id] as $attendee_title => $attendee_data) {
 					$participation_type = '';
 					$secret_code = '';
+					$first_name = '';
+					$last_name = '';
 					foreach( $attendee_data as $attendee_label => $attendee_value) {
 						$label = apply_filters('translate_text', $attendee_label, 'ru');
+						if ($label == 'Имя (на английском)') {
+							$first_name = $attendee_value;
+						}
+						if ($label == 'Фамилия (на английском)') {
+							$last_name = $attendee_value;
+						}
 						if ($label == 'Участвует в качестве') {
 							$participation_type = apply_filters('translate_text', $attendee_value, 'ru');
 						}
 						if ($label == 'Секретный код') {
-							$secret_code = $attendee_value;
+							$secret_code = trim($attendee_value);
                             $secret_code_needed = true;
 						}
 					}
@@ -112,7 +132,7 @@ class EM_Limmud_Secret {
 						continue;
 					}
 
-					if (!self::check_code_unique($secret_code, $EM_Booking)) {
+					if (!self::check_code_unique($secret_code, $first_name, $last_name, $EM_Booking)) {
 						$EM_Booking->add_error(__('[:en]Secret code[:ru]Секретный код[:he]קוד סודי[:] ' . $secret_code . ' [:en]was already used[:ru]уже был использован[:he]כבר היה בשימוש[:]'));
 						$secret_code_invalid = true;
 						continue;
